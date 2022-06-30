9ad974646a10cca341c460b9ebab216e7ea08164
andres <mucino.andres1990@gmail.com>
2022-06-30 17:43:30 -0500

    modific component chechbox

--- src/components/Checkbox/Checkbox.stories.tsx
+++ src/components/Checkbox/Checkbox.stories.tsx
@@ -1,23 +1,31 @@
 import { ComponentMeta, ComponentStory } from "@storybook/react";
 import React from "react";
-import { AtomCheckbox } from "./Checkbox";
+import { AtomCheckbox, type } from "./Checkbox";
 
 export default {
   title: "Atoms/Checkboxs",
   component: AtomCheckbox,
+  argTypes: {
+    type: {
+      options: Object.keys(type),
+      mapping: type,
+      control: { type: "select" },
+    },
+  },
 } as ComponentMeta<typeof AtomCheckbox>;
 
-export const TemplateAtomCheckbox: ComponentStory<typeof AtomCheckbox> = (
+export const Checkbox: ComponentStory<typeof AtomCheckbox> = (
   args
 ) => {
   const [checked, setChecked] = React.useState(false);
 
   return (
     <AtomCheckbox
-      label={'checkbox'}
+      label={args.label || "checkbox"}
       onChange={(e) => setChecked(e.target.checked)}
       checked={checked}
       disabled={args.disabled}
+      type={args.type || 'checkbox'}
     />
   );
 };
--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -2,30 +2,81 @@ interface AtomCheckboxProps {
   label?: string;
   checked: boolean;
   disabled?: boolean;
-  onChange?: React.ChangeEventHandler<HTMLInputElement>;
-  // color: string;
+  onChange?: (e: any) => void;
+  type?: string;
 }
+
+export const type = {
+  switch: "switch",
+  radio: "radio",
+  checkbox: "checkbox",
+};
 
 export const AtomCheckbox = ({
   disabled,
   label,
   onChange,
   checked,
+  type,
 }: AtomCheckboxProps) => {
-  const CONTAINER_CHECKBOX = `flex items-center space-x-2.5`;
-  const INPUT_CHECKBOX = `w-5 h-5`;
-  const LABEL_CHECKBOX = `${disabled === true ? "text-gray-400" : ""}`;
+  const CONTAINER_CHECKBOX = `flex items-center space-x-2.5 `;
+
+  const INPUT_CHECKBOX = `w-5 h-5 cursor-pointer focus:ring-transparent`;
+
+  const LABEL_CHECKBOX = `${disabled ? "text-gray-400" : ""}`;
+
+  const CONTAINER = `border-2 w-10 h-4.5 rounded-full ${
+    checked ? "bg-blue-500 duration-700" : "bg-gray-200 duration-700"
+  }  py-0.5 flex aling-items-center ${disabled && "bg-gray-300"}`;
+
+  const CIRCLE = ` w-3 h-3 px-1 bg-white rounded-full ${
+    checked ? "translate-x-6 duration-700" : "duration-700"
+  }`;
+
+  const INPUT = `w-0 h-0`;
+
+  const INPUT_RADIO = `w-5 h-5 relative border rounded-full flex justify-center items-center ${
+    checked && "border-blue-500"
+  }`;
+
+  const CIRCLE_RADIO = `w-3 h-3 rounded-full absolute ${
+    checked && "bg-blue-500"
+  }`;
 
   return (
     <div className={CONTAINER_CHECKBOX}>
-      <input
-        type="checkbox"
-        className={INPUT_CHECKBOX}
-        disabled={disabled}
-        onChange={onChange}
-        checked={checked}
-      />
-      <label className={LABEL_CHECKBOX}>{label}</label>
+      {type === "switch" ? (
+        <label className={CONTAINER}>
+          <input
+            type="checkbox"
+            className={INPUT}
+            checked={checked}
+            onChange={onChange}
+            disabled={disabled}
+          />
+          <span className={CIRCLE}></span>
+        </label>
+      ) : type === "checkbox" ? (
+        <input
+          type={"checkbox"}
+          className={INPUT_CHECKBOX}
+          disabled={disabled}
+          onChange={onChange}
+          checked={checked}
+        />
+      ) : (
+        <label className={INPUT_RADIO}>
+          <input
+            type="checkbox"
+            className={INPUT}
+            checked={checked}
+            onChange={onChange}
+            disabled={disabled}
+          />
+          <span className={CIRCLE_RADIO}></span>
+        </label>
+      )}
+      <span className={LABEL_CHECKBOX}>{label}</span>
     </div>
   );
 };
